3814843d5df4d584fd2cd94807bd6e715dce9114
Minjae jin <minjaejin@Minjaes-MacBook-Air.local>
2024-04-18 15:23:47 +0900

    second git commit

--- map.py
+++ map.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 import time
 
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 #from mpl_toolkits.mplot3d import Axes3D
 import seaborn as sns
 
